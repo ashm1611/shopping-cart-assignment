0f5765d245cbaa6b4b3a4b85336e61b357c0f497
Ashish Mishra <ashmishr9@WKMZTC490048.global.publicisgroupe.net>
2023-05-17 08:58:44 +0530

    Initial Setup : ESLINT, Prettier, Context Provider & Reducer

---
+++ src/context/Context.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect, createContext, useReducer,  } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import { cartReducers } from "../reducers/cartReducers";
+import Products from "../server/products/index.get.json";
+import Categories from "../server/categories/index.get.json";
+
+const initialState = {
+  count: 0,
+  products: {},
+};
+
+export const GlobalContext = createContext(initialState);
+
+const Context = ({ children }) => {
+  const [cartItems, dispatch] = useReducer(cartReducers, initialState);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const filteredCategory = Categories.filter(
+    (category) => category.enabled
+  ).sort((a, b) => a.order - b.order);
+
+  const [filteredProduct, setFilteredProduct] = useState(Products);
+
+  useEffect(() => {
+    if (!location.search) {
+      setFilteredProduct(Products);
+    }
+  }, [location]);
+
+  function handleProduct(id) {
+    setFilteredProduct(Products.filter((product) => product.category === id));
+    navigate({
+      pathname: `/products`,
+      search: `?query=${id}`,
+    });
+    window.scrollTo(0, 0);
+  }
+
+  return (
+    <GlobalContext.Provider value={{ cartItems, dispatch, filteredProduct, handleProduct, filteredCategory  }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export default Context;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from "./App";
+import "./styles/_base.scss";
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from "react-router-dom";
+import GlobalProvider from "./context/Context";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
